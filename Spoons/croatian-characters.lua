f88f868ad2c1358c7df5ec96de8d16c0a0ea2489
Bero <bero@Bero-A8C-Mac.local>
2019-12-03 17:49:48 +0100

    Croatian character keybindings

---
+++ Spoons/croatian-characters.lua
@@ -0,0 +1,38 @@
+hs.hotkey.bind({'alt', 'ctrl'}, '[', function()
+  hs.eventtap.keyStrokes('š')
+end)
+hs.hotkey.bind({'alt', 'ctrl', 'shift'}, '[', function()
+  hs.eventtap.keyStrokes('Š')
+end)
+
+
+hs.hotkey.bind({'alt', 'ctrl'}, ']', function()
+  hs.eventtap.keyStrokes('đ')
+end)
+hs.hotkey.bind({'alt', 'ctrl', 'shift'}, ']', function()
+  hs.eventtap.keyStrokes('Đ')
+end)
+
+
+hs.hotkey.bind({'alt', 'ctrl'}, '\\', function()
+  hs.eventtap.keyStrokes('ž')
+end)
+hs.hotkey.bind({'alt', 'ctrl', 'shift'}, '\\', function()
+  hs.eventtap.keyStrokes('Ž')
+end)
+
+
+hs.hotkey.bind({'alt', 'ctrl'}, ';', function()
+  hs.eventtap.keyStrokes('č')
+end)
+hs.hotkey.bind({'alt', 'ctrl', 'shift'}, ';', function()
+  hs.eventtap.keyStrokes('Č')
+end)
+
+
+hs.hotkey.bind({'alt', 'ctrl'}, '\'', function()
+  hs.eventtap.keyStrokes('ć')
+end)
+hs.hotkey.bind({'alt', 'ctrl', 'shift'}, '\'', function()
+  hs.eventtap.keyStrokes('Ć')
+end)
---
+++ init.lua
@@ -0,0 +1,4 @@
+require 'Spoons/croatian-characters'
+require 'Spoons/utility'
+
+hs.alert.show('Config loaded')
