809b738c348cbd504bba608995c5ffdd8ff9264a
Bero <berislav.grgicak@gmail.com>
2019-12-21 16:12:55 +0100

    launch finder

--- Spoons/utility.lua
+++ Spoons/utility.lua
@@ -1,3 +1,18 @@
+-- Launch Finder.
+
+hs.hotkey.bind({'alt'}, 'f', function()
+  appName = 'Finder'
+  app = hs.application.get( appName )
+  if( app:isFrontmost() )
+  then
+    app:hide()
+  else
+    app.launchOrFocus( appName )
+  end
+end)
+
+-- Reload Hammerspoon
+
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'R', function()
   hs.reload()
 end)
